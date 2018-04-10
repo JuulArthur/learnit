6ef1bc83fe40e0428f9178e38d3ba67154edf42c
Juul Rudihagen <juularthur92@gmail.com>
2018-04-10 16:26:31 +0200

    Change name

--- client/src/Home/Home.js
+++ client/src/Home/Home.js
@@ -4,7 +4,7 @@ class Home extends Component {
     render() {
         return (
             <div className="Home">
-                <h1 className="header">Welcome to Learn It</h1>
+                <h1 className="header">Welcome to We Ignite Tech</h1>
                 <h2 className="secondary-header">Learn It, Build It, Make It</h2>
             </div>
         );
